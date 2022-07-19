240c68c1c5b7adca050592f0622b91b165d9595e
“tmxlavor813” <tmxlavor813@naver.com>
2022-07-19 11:31:34 +0900

    람다 추가

--- ifelse.py
+++ ifelse.py
@@ -22,11 +22,10 @@ lst = [1,2,3]
 for item in lst:
     print(item)
 
-lst = list
-(range(1,11))
+lst = list(range(1,11))
 print(lst)
 for i in lst:
-    if i >5:
+    if i > 5:
         break
     print("Item:{0}".format(i))
 
@@ -36,3 +35,30 @@ for i in lst:
         continue
     print("item{0}".format(i))
 
+print("---range함수---")
+print(list(range(2000,2023)))
+print("---리스트 내장(임베딩, 컴프리헨션)---")
+lst = list(range(1,11))
+print([1**2 for i in lst if i > 5])
+tp=("apple","kiwi", "banana")
+print([len(i) for i in tp])
+
+print("---필터링---")
+lst = [10,25,30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+#조건이 되는 함수 정의
+def getBiggerThan20(i):
+    return i > 20
+
+print("---필터링 함수 사용---")
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+print("---람다 함수 사용---")
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print(item)
