486d8bae6f3b870d21b34ffbcd1d9617282b4c19
“tmxlavor813” <tmxlavor813@naver.com>
2022-07-19 14:53:18 +0900

    class 7

---
+++ class1.py
@@ -0,0 +1,21 @@
+# class1.py
+#1)클래스 정의
+class Person:
+    #초기화 메서드(생성자)
+    def __init__(self):
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+#2)인스턴스 
+p1 = Person()
+p2 = Person()
+p1.name="전우치"
+#3)인스턴스 메서드 호출
+p1.print()
+p2.print()
+
+person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
--- 생성자소멸자01.py
+++ 생성자소멸자01.py
@@ -3,6 +3,12 @@ class MyClass:
     def __init__(self, value):
         self.value = value
         print("Instace is created! value = ", value)
+    #소멸자 메서드
     def __del__(self):
         print("Instance is deleted!")
 
+#인스턴트 생성
+d = MyClass(5)
+#메모리에서 제거
+#del d
+print("젠체 코드 실행 종료")
