af7bff76eaa5f991b7a3aaed5a879ad694c09932
“tmxlavor813” <tmxlavor813@naver.com>
2022-07-20 14:20:39 +0900

    파일 입출력 2

--- DemoRE.py
+++ DemoRE.py
@@ -9,3 +9,11 @@ print(result.group())
 result = re.match("[0-9]*th", "35th")
 print(result)
 print(result.group())
+
+print(bool(re.search("apple","this is apple")))
+print(bool(re.match("apple","this is apple")))
+
+result = re.search("\d{4}", "올해는 2022년 입니다.")
+print(result.group())
+result = re.search("\d{5}", "우리 동네는 52100입니다.")
+print(result.group())
