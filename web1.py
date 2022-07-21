4d72dcca614c2b8838ed00207d22c59535620ebe
“tmxlavor813” <tmxlavor813@naver.com>
2022-07-21 10:39:32 +0900

    웹 크롤링 연습 1

---
+++ web1.py
@@ -0,0 +1,9 @@
+#web1.py
+#웹 크롤링을 위한 라이브러리
+from bs4 import BeautifulSoup
+
+#페이지 로딩
+page = open("test.html", "rt", encoding="utf-8").read()
+#검색이 용이한 객체
+soup = BeautifulSoup(page, "html.parser")
+print(soup.prettify())
