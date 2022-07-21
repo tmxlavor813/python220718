546bdc3e72379e242b199891d0e02dadb0ad083c
“tmxlavor813” <tmxlavor813@naver.com>
2022-07-21 11:21:20 +0900

    네비어 웹툰 리스트

--- web1.py
+++ web1.py
@@ -3,7 +3,21 @@
 from bs4 import BeautifulSoup
 
 #페이지 로딩
-page = open("test.html", "rt", encoding="utf-8").read()
+page = open("test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+#find_all()메서드
+#<p> 전체를 검색
+#print(soup.find_all("p"))
+#<a>태그
+#print(soup.find_all("a"))
+#find()는 한개를 검색
+#print(soup.find("p"))
+#필터링: <p class='outer-text'>인 경우
+#print(soup.find_all("p", class_='outer-text'))
+#태그의 내부 문자열을 리턴
+for tag in soup.find_all("P"):
+    title = tag.text.strip()
+    title = title.replace("\n", "")
+    print(title)
